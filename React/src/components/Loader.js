4a040ff2177a5cee674208304d0218e6071f4278
Sagar Somangoudar <hssagar8@gmail.com>
2025-07-31 15:17:34 -0500

    Add files via upload

---
+++ React/src/components/Loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { Container } from "@mui/material";
+
+
+export default function Loader() {
+  return (
+    <Container
+      maxWidth="sm"
+      fixed
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >
+      <Box>
+        <CircularProgress />
+      </Box>
+    </Container>
+  );
+}
---
+++ React/src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+/*
+This acts a proxy between the react application and the flask microservice
+Everytime there is a request to /api, the setupProxy prepends the flask
+microservice url mentioned in line 14
+*/
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      // update the flask Google Cloud url
+
+      target: "https://flaskdemo-106523648147.us-central1.run.app",
+      changeOrigin: true,
+    })
+  );
+};
